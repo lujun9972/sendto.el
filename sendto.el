076b81d7a53f75b0a59b0ef3448f35570567054c
lujun9972 <lujun9972@gmail.com>
2016-04-25 20:50:19 +0800

    Copyright

--- sendto.el
+++ sendto.el
@@ -1,6 +1,6 @@
 ;;; sendto.el --- send the region content to a function -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2004-2015 DarkSun <lujun9972@gmail.com>
+;; Copyright (C) 2004-2016 DarkSun <lujun9972@gmail.com>
 
 ;; Author: DarkSun <lujun9972@gmail.com>
 ;; Created: 2016-4-22
